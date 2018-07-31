e5de690794413e472ce1e4ab93279053c69e331e
Joseph Stalin <iamjosephstalin@outlook.com>
2018-07-31 16:41:13 +0530

    Add files

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/store/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/store/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/XAMPP/xamppfiles/htdocs/store/catalog/');
+define('DIR_SYSTEM', '/Applications/XAMPP/xamppfiles/htdocs/store/system/');
+define('DIR_IMAGE', '/Applications/XAMPP/xamppfiles/htdocs/store/image/');
+define('DIR_LANGUAGE', '/Applications/XAMPP/xamppfiles/htdocs/store/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/XAMPP/xamppfiles/htdocs/store/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/XAMPP/xamppfiles/htdocs/store/system/config/');
+define('DIR_CACHE', '/Applications/XAMPP/xamppfiles/htdocs/store/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/XAMPP/xamppfiles/htdocs/store/system/storage/download/');
+define('DIR_LOGS', '/Applications/XAMPP/xamppfiles/htdocs/store/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/XAMPP/xamppfiles/htdocs/store/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/XAMPP/xamppfiles/htdocs/store/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'pavo_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
